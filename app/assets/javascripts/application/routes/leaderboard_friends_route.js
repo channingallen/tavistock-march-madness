faecde6ec4e1ea9533aa93bcf37480c38b37c09b
Courtland Allen <courtlandallen@gmail.com>
2013-02-20 01:17:59 -0800

    Fixes to the refreshing + initial loading of content for the leaderboard "you" page.

--- app/assets/javascripts/application/routes/leaderboard_friends_route.js
+++ app/assets/javascripts/application/routes/leaderboard_friends_route.js
@@ -4,6 +4,21 @@ App.LeaderboardFriendsRoute = Ember.Route.extend({
     if (!App.get('currentUser')) this.transitionTo('leaderboard.top');
   },
 
+  model: function() {
+    var fbIds = [App.get('currentUser').get('fbId')],
+      friendIds = App.get('friendIds');
+    if (friendIds) {
+      for (var i = 0; i < friendIds.length; i++) {
+        fbIds.push(friendIds[i]);
+      }
+    }
+    return App.User.find({ fb_id: fbIds});
+  }.observes('App.friendIds'),
+
+  setupController: function(controller, model) {
+    controller.set('content', model);
+  }.observes('App.friendIds'),
+
   renderTemplate: function() {
     this.render('leaderboard/leaderboard_rankings');
   }
