4420ea2016d2014bbbb6dc15a53a61e2670ea683
Courtland Allen <courtlandallen@gmail.com>
2013-02-21 23:43:55 -0800

    Enforcing form page completion before access to the rest of the app is allowed.

--- app/assets/javascripts/application/routes/bracket_route.js
+++ app/assets/javascripts/application/routes/bracket_route.js
@@ -1,7 +1,14 @@
 App.BracketRoute = Ember.Route.extend({
+
+  redirect: function() {
+    var currentUser = App.get('currentUser');
+    if (!currentUser.get('email')) this.transitionTo('form');
+  },
+
   model: function(params) {
     return App.Bracket.find(params.bracket_id);
   },
+
   setupController: function(controller, model) {
     controller.set('content', model);
   }
--- app/assets/javascripts/application/routes/rules_route.js
+++ app/assets/javascripts/application/routes/rules_route.js
@@ -1,1 +1,7 @@
-App.RulesRoute = Ember.Route.extend({});
+App.RulesRoute = Ember.Route.extend({
+
+  redirect: function() {
+    var currentUser = App.get('currentUser');
+    if (!currentUser.get('email')) this.transitionTo('form');
+  }
+});
