c1621603e3eb94cf1006646fae928d2244c170f7
Courtland Allen <courtlandallen@gmail.com>
2013-02-22 02:29:09 -0800

    Implemented form.

--- lib/constants.rb
+++ lib/constants.rb
@@ -9,66 +9,91 @@ module Constants
   # Restaurants
   RESTAURANTS = {
     "265297633509254" => {
+      :id => "265297633509254",
+      :id => "265297633509254",
       :name => "Joe's American Bar and Grille",
-      :image => "like_gates/like_gate_joes.png" 
+      :image => "like_gates/like_gate_joes.png"
     },
     "171824636186609" => {
+      :id => "171824636186609",
       :name => "Napa Valley Grille",
-      :image => "like_gates/like_gate_napa.png"
+      :image => "like_gates/like_gate_napa.png",
+      :location => "Bloomington, MN"
     },
     "58761811526" => {
+      :id => "58761811526",
       :name => "Napa Valley Grille",
-      :image => "like_gates/like_gate_napa.png"
+      :image => "like_gates/like_gate_napa.png",
+      :location => "Los Angeles, CA"
     },
     "181834148524304" => {
+      :id => "181834148524304",
       :name => "Alcatraz Brewing Co",
       :image => "like_gates/like_gate_alcatraz.png"
     },
     "347993055268" => {
+      :id => "347993055268",
       :name => "Blackhawk Grille",
       :image => "like_gates/like_gate_blackhawk.png"
     },
     "92945941516" => {
+      :id => "92945941516",
       :name => "California Cafe",
-      :image => "like_gates/like_gate_california.png"
+      :image => "like_gates/like_gate_california.png",
+      :location => "Los Gatos, CA"
     },
     "52194381546" => {
+      :id => "52194381546",
       :name => "California Cafe",
-      :image => "like_gates/like_gate_california.png"
+      :image => "like_gates/like_gate_california.png",
+      :location => "Palo Alto, CA"
     },
     "130416646986919" => {
+      :id => "130416646986919",
       :name => "Sapporo",
       :image => "like_gates/like_gate_sapporo.png"
     },
     "41150308569" => {
+      :id => "41150308569",
       :name => "ZED451",
       :image => "like_gates/like_gate_zed.png"
     },
     "71191159109" => {
+      :id => "71191159109",
       :name => "Timpano",
-      :image => "like_gates/like_gate_timpano.png"
+      :image => "like_gates/like_gate_timpano.png",
+      :location => "Rockville, MD"
     },
     "92682632632" => {
+      :id => "92682632632",
       :name => "Timpano",
-      :image => "like_gates/like_gate_timpano.png"
+      :image => "like_gates/like_gate_timpano.png",
+      :location => "Fort Lauderdale, FL"
     },
     "74800812234" => {
+      :id => "74800812234",
       :name => "Timpano",
-      :image => "like_gates/like_gate_timpano.png"
+      :image => "like_gates/like_gate_timpano.png",
+      :location => "Orlando, FL"
     },
     "79529882826" => {
+      :id => "79529882826",
       :name => "Timpano",
-      :image => "like_gates/like_gate_timpano.png"
+      :image => "like_gates/like_gate_timpano.png",
+      :location => "Tampa, FL"
     },
     "190166997684083" => {
+      :id => "190166997684083",
       :name => "Abe's",
       :image => "like_gates/like_gate_abe.png"
     },
     "120870517924902" => {
+      :id => "120870517924902",
       :name => "Atlantic Fish",
       :image => "like_gates/like_gate_atlantic.png"
     },
     "486859618037849" => {
+      :id => "486859618037849",
       :name => "Bracket Challenge Community",
       :image => "backgrounds/courtside.png"
     }
