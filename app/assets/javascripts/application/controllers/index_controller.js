fc46d08f5c4a15a6d824e1e7111ce1f615556c2f
Courtland Allen <courtlandallen@gmail.com>
2013-02-22 19:10:06 -0800

    Updated CSS for index page + linking to official brackets for admins.

--- app/assets/javascripts/application/controllers/index_controller.js
+++ app/assets/javascripts/application/controllers/index_controller.js
@@ -3,6 +3,15 @@ App.IndexController = Ember.Controller.extend({
   currentUser: function() {
     this.set('content', App.get('currentUser'));
     return this.get('content');
-  }.property('App.currentUser')
+  }.property('App.currentUser'),
+
+  isAdmin: function() {
+    var user = this.get('content');
+    return _.contains(App.get('adminFbIds'), user.get('fbId'));
+  }.property('content.fbId'),
+
+  officialBracket: function() {
+    return App.Bracket.find(1);
+  }.property('content')
 
 });
--- app/assets/javascripts/application/setup.js
+++ app/assets/javascripts/application/setup.js
@@ -6,6 +6,12 @@ window.App = Ember.Application.create({
   // An array of the Facebook IDs of the user's Facebook friends.
   friendIds: [],
 
+  // An array of the Facebook IDs of admin accounts.
+  // 707419 - Courtland Allen
+  // 4930013 - Channing Allen
+  // 1038690087 - Melissa Chen
+  adminFbIds: ['707419', '4930013', '1038690087'],
+
   ready: function() {
     App.bindings();
   }
